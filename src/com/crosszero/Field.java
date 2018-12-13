f0d51ab70839ebd530d61977abf1a2dc2f41065a
Alexander Kuznetsov <_ananas@mail.ru>
2018-12-13 17:25:31 +0300

    initial

---
+++ src/com/crosszero/Field.java
@@ -0,0 +1,17 @@
+package com.crosszero;
+
+public class Field {
+
+    int cells[] = new int[8];
+
+    public void printCells() {
+        System.out.println(cells[0]);
+        System.out.println(cells[1]);
+        System.out.println(cells[2]);
+        System.out.println(cells[3]);
+        System.out.println(cells[4]);
+        System.out.println(cells[5]);
+        System.out.println(cells[6]);
+        System.out.println(cells[7]);
+    }
+}
---
+++ src/com/crosszero/GameRunner.java
@@ -0,0 +1,9 @@
+package com.crosszero;
+
+public class GameRunner {
+
+    public static void main(String[] args) {
+        Field field = new Field();
+        field.printCells();
+    }
+}
